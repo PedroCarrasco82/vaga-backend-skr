480c018d91bc060c377ce75eed53f20069f97eb4
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-10-04 21:53:00 -0300

    add: setup handler 🎉

--- data-scrap-process/src/index.ts
+++ data-scrap-process/src/index.ts
@@ -3,11 +3,6 @@ import {Normalization} from "./normalization";
 import {Scrapper} from "./scrapper";
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World");
-});
-
 export const scrapper = functions.https.onRequest(
     (request, response) => new Scrapper({request, response}).run()
 );
