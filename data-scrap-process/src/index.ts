bac4d683eff31183fae850ad12c08fb1fbd2bc07
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-10-06 09:43:03 -0300

    feat: data persistence in migration✨

--- data-scrap-process/src/index.ts
+++ data-scrap-process/src/index.ts
@@ -3,6 +3,7 @@ import {Scrapper} from "./scrapper";
 import * as functions from "firebase-functions";
 
 const functionConfigs: functions.RuntimeOptions = {
+  timeoutSeconds: 300,
   memory: "1GB",
 };
 
@@ -11,7 +12,6 @@ export const scrapper = functions.runWith(functionConfigs).https.onRequest(
       await new Scrapper({request, response}).run()
 );
 
-export const migration = functions.runWith(functionConfigs).https.onRequest(
-    async (request, response) =>
-      await new Migration({request, response}).run()
-);
+export const migration = functions
+    .runWith(functionConfigs)
+    .https.onRequest(async (request, response) => await new Migration({request, response}).run());
--- data-scrap-process/src/migration/migration.ts
+++ data-scrap-process/src/migration/migration.ts
@@ -1,15 +1,49 @@
 import IHttpHandler from "../interfaces/httpHandler";
 import IFunction from "../interfaces/function";
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 
 export class Migration implements IFunction {
-  constructor(private httpHandler: IHttpHandler) {}
+  database: FirebaseFirestore.Firestore;
 
-  public run(): void {
-    const {response} = this.httpHandler;
+  constructor(private httpHandler: IHttpHandler) {
+    if (!admin.apps.length) {
+      admin.initializeApp();
+    }
+    this.database = admin.firestore();
+    this.database.settings({ignoreUndefinedProperties: true});
+  }
+
+  public async run(): Promise<void> {
+    const {request, response} = this.httpHandler;
 
     functions.logger.info("Initialize migration", {structuredData: true});
-    response.send("Migration");
+    const body = JSON.parse(request.body);
+
+    const buildsCollection = this.database.collection("builds");
+    await this.storeData(body, buildsCollection);
+    response.send("Finish :)");
+  }
+
+  private async storeData(scrapData: any[], buildsCollection: FirebaseFirestore.CollectionReference) {
+    for (const itemScrapData of scrapData) {
+      const itemExistsQuerySnapshot = await buildsCollection
+          .where("name", "==", itemScrapData["name"])
+          .where("localization", "==", itemScrapData["localization"]).get();
+
+      let itemExists: any;
+      itemExistsQuerySnapshot.forEach((doc) => {
+        itemExists = {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      if (itemExists) {
+        buildsCollection.doc(itemExists.id).update(itemScrapData);
+      } else {
+        buildsCollection.add(itemScrapData);
+      }
+    }
   }
 }
